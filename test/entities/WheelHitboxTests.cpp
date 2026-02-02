1929e12ecabd1e42d330484532d17a8eea446a43
danwflynn <flynn.da@northeastern.edu>
2026-02-01 22:30:49 -0500

    transform test with wheel hitbox

--- test/entities/WheelHitboxTests.cpp
+++ test/entities/WheelHitboxTests.cpp
@@ -13,7 +13,8 @@
 class WheelHitboxTests : public ::testing::Test {
 protected:
     Transform transform;
-    std::optional<WheelHitbox> wheel;
+    std::optional<WheelHitbox> frontWheel;
+    std::optional<WheelHitbox> backWheel;
     
     WheelHitboxTests() {}
 
@@ -35,11 +36,15 @@ protected:
 
             if (mesh->mName == aiString("polygon2")) {
                 std::vector<glm::vec3> posVertices;
+                std::vector<glm::vec3> negVertices;
                 posVertices.reserve(mesh->mNumVertices);
+                negVertices.reserve(mesh->mNumVertices);
                 for (unsigned int v = 0; v < mesh->mNumVertices; v++) {
                     if (mesh->mVertices[v].z >= 0) posVertices.push_back(glm::vec3(mesh->mVertices[v].x, mesh->mVertices[v].y, mesh->mVertices[v].z));
+                    else negVertices.push_back(glm::vec3(mesh->mVertices[v].x, mesh->mVertices[v].y, mesh->mVertices[v].z));
                 }
-                wheel.emplace(posVertices, transform);
+                frontWheel.emplace(posVertices, transform);
+                backWheel.emplace(negVertices, transform);
             }
         }
         transform.SetScale(glm::vec3(Bike::SCALE));
@@ -47,7 +52,19 @@ protected:
 };
 
 
-TEST_F(WheelHitboxTests, GetWorldCenterWithTransformAtOrigin) {
-    ASSERT_TRUE(wheel.has_value());
-    std::cout << glm::to_string(wheel->GetWorldCenter()) << std::endl;
+TEST_F(WheelHitboxTests, GetWorldCenterAlignedWithTransform) {
+    ASSERT_TRUE(frontWheel.has_value());
+    glm::vec3 originalFrontWorldCenter = frontWheel->GetWorldCenter();
+    glm::vec3 originalBackWorldCenter = backWheel->GetWorldCenter();
+    std::cout << "Starting front wheel center: " << glm::to_string(frontWheel->GetWorldCenter()) << std::endl;
+    std::cout << "Starting back wheel center: " << glm::to_string(backWheel->GetWorldCenter()) << std::endl;
+    transform.SetPosition(glm::vec3(1.0f, 1.0f, 1.0f));
+    EXPECT_FLOAT_EQ(frontWheel->GetWorldCenter().x, originalFrontWorldCenter.x + 1.0f);
+    EXPECT_FLOAT_EQ(frontWheel->GetWorldCenter().y, originalFrontWorldCenter.y + 1.0f);
+    EXPECT_FLOAT_EQ(frontWheel->GetWorldCenter().z, originalFrontWorldCenter.z + 1.0f);
+    EXPECT_FLOAT_EQ(backWheel->GetWorldCenter().x, originalBackWorldCenter.x + 1.0f);
+    EXPECT_FLOAT_EQ(backWheel->GetWorldCenter().y, originalBackWorldCenter.y + 1.0f);
+    EXPECT_FLOAT_EQ(backWheel->GetWorldCenter().z, originalBackWorldCenter.z + 1.0f);
+    std::cout << "Ending front wheel center: " << glm::to_string(frontWheel->GetWorldCenter()) << std::endl;
+    std::cout << "Ending back wheel center: " << glm::to_string(backWheel->GetWorldCenter()) << std::endl;
 }
